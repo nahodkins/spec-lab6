382ffe78a7dee18ecde33f1356b08ca4c1016fa4
nahodkins <tsuber2001@gmail.com>
2021-11-21 20:08:47 +0200

    Adds 4th task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
 
+    private static final String BYTES_FILE_NAME = "bytes.txt";
+    private static final String DELIMITER = "+-------+-------+------+";
+    private static final String OUTPUT_HEADER = DELIMITER + "\n| Index | Given | Read |\n" + DELIMITER;
+    private static final String OUTPUT_TEMPLATE = "|  %2d   |  %2s   | %2s   |\n" + DELIMITER;
+
+
+    private static byte[] initializeByteArray(int size) {
+        byte[] bytes = new byte[size];
+        for (int i = 0; i < size; i++) {
+            bytes[i] = (byte) ((Math.random() * 4) + 1);
+        }
+        return bytes;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        byte[] bytes = initializeByteArray(64);
+        RandomAccessIO.writeBytes(BYTES_FILE_NAME, bytes);
+        RandomAccessIO.invertEvenBytes(BYTES_FILE_NAME);
+        byte[] readBytes = RandomAccessIO.readBytes(BYTES_FILE_NAME);
+        System.out.println(OUTPUT_HEADER);
+        IntStream.range(0, bytes.length)
+                .mapToObj(i -> String.format(OUTPUT_TEMPLATE, i, bytes[i], readBytes[i]))
+                .forEach(System.out::println);
     }
 }
---
+++ src/main/java/RandomAccessIO.java
@@ -0,0 +1,46 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class RandomAccessIO {
+
+    private static final String READ_WRITE_OPTION = "rw";
+
+    public static byte[] readBytes(String filePath) {
+        byte[] bytes;
+        try (RandomAccessFile file = new RandomAccessFile(filePath, READ_WRITE_OPTION)) {
+            bytes = new byte[(int) file.length()];
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = file.readByte();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+        return bytes;
+    }
+
+    public static void writeBytes(String filePath, byte[] bytes) {
+        try (RandomAccessFile file = new RandomAccessFile(filePath, READ_WRITE_OPTION)){
+            for (byte b : bytes) {
+                file.write(b);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void invertEvenBytes(String filePath) {
+        byte[] bytes = readBytes(filePath);
+        try (RandomAccessFile file = new RandomAccessFile(filePath, READ_WRITE_OPTION)) {
+            for (int i = 0; i < bytes.length; i += 2) {
+                file.seek(i);
+                file.write(bytes[i] * -1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
